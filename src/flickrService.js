51d80681579533022043d5c1625e7ce5615271e8
tamburkey <petem14@gmail.com>
2020-08-22 15:01:25 -0400

    add .env

--- src/flickrService.js
+++ src/flickrService.js
@@ -2,7 +2,7 @@ function flickrService(state) {
     const proxyServer = "https://shrouded-mountain-15003.herokuapp.com/"
     const domain = "https://flickr.com"
     const path = "/services/rest/"
-    const apiKey = "d354ec6250a62eb1abebd8a6501e836c"
+    const apiKey = process.env.REACT_APP_API_KEY
     const photoCount = 5
     const queryString = `api_key=${apiKey}&format=json&nojsoncallback=1`
     + `&method=flickr.photos.search&safe_search=1&per_page=${photoCount}`
