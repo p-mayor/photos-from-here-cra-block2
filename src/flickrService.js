ea08d2821ff58fba9a567c59a0537007b88acf79
tamburkey <petem14@gmail.com>
2020-08-22 14:54:59 -0400

    refactor querystring

--- src/flickrService.js
+++ src/flickrService.js
@@ -4,10 +4,11 @@ function flickrService(state) {
     const path = "/services/rest/"
     const apiKey = "d354ec6250a62eb1abebd8a6501e836c"
     const photoCount = 5
+    const queryString = `api_key=${apiKey}&format=json&nojsoncallback=1`
+    + `&method=flickr.photos.search&safe_search=1&per_page=${photoCount}`
+    + `&lat=${state.lat}&lon=${state.lon}&text=${state.searchTerm}`
 
-    const URL = `${proxyServer + domain + path}?api_key=${apiKey}&format=json&nojsoncallback=1`
-        + `&method=flickr.photos.search&safe_search=1&per_page=${photoCount}`
-        + `&lat=${state.lat}&lon=${state.lon}&text=${state.searchTerm}`
+    const URL = `${proxyServer + domain + path}?${queryString}`
 
     return fetch(URL).then((response) => {
         return response.json()
