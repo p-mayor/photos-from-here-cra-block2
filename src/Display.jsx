c18f1fbcc662b959cee7319f54eb7b35c1193eb0
tamburkey <petem14@gmail.com>
2020-08-23 14:56:17 -0400

    format / title / remove svg

--- src/Display.jsx
+++ src/Display.jsx
@@ -32,7 +32,7 @@ class Display extends React.Component {
             this.setState({
                 lat: location.coords.latitude,
                 lon: location.coords.longitude
-            }, ()=>{
+            }, () => {
                 this.getPictures()
                 this.geocodeLocation()
             })
@@ -44,7 +44,7 @@ class Display extends React.Component {
 
         navigator.geolocation.getCurrentPosition(onSuccess, onFail)
 
-        
+
     }
 
     // get pictures from the flickr api
